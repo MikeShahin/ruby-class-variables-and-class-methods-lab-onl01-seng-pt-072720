05364dd4f186a8999ad062170d3e8ffbe01c3fcf
Mike Shahindoust <gobias101@gmail.com>
2020-08-15 00:38:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,16 +5,13 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   def initialize(name, artist, genre)
-    @name
-    @artist
-    @genre
+    @name = name
+    @artist = artist
+    @genre = genre
     
     @@count += 1
   end
   
-  def name
-    @name
-  end
   def self.count
     @@count
   end

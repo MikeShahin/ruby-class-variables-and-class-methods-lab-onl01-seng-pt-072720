5fd62886e5c76f55c9ed678c42140927af062158
Mike Shahindoust <gobias101@gmail.com>
2020-08-15 00:58:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,14 +42,14 @@ class Song
   end
   
    def self.artist_count
-    artist_count = {}
+    artists = {}
     @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1 
+      if artists[artist]
+        artists[artist] += 1 
       else
-        artist_count[artist] = 1
+        artists[artist] = 1
       end
     end
-    artist_count
+    artists
   end
 end
